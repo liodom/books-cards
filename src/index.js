b1dd0ee6ca11cf25be46d22ebf4416c35cb2d798
liodom <33369653+liodom@users.noreply.github.com>
2020-10-10 22:20:42 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const data = [
+  {
+    title: "Il nuovo Devoto-Oli junior. Il mio primo vocabolario di italiano.",
+    author: "Giacomo Devoto",
+    imgURL:
+      "https://images-eu.ssl-images-amazon.com/images/I/71ncGHw19PL._AC_UL200_SR200,200_.jpg",
+    objectId: 1,
+  },
+  {
+    title: "Il nuovo Devoto-Oli junior. Il mio primo vocabolario di italiano.",
+    author: "Giacomo Devoto",
+    imgURL:
+      "https://images-eu.ssl-images-amazon.com/images/I/71ncGHw19PL._AC_UL200_SR200,200_.jpg",
+    objectId: 2,
+  },
+  {
+    title: "Il nuovo Devoto-Oli junior. Il mio primo vocabolario di italiano.",
+    author: "Giacomo Devoto",
+    imgURL:
+      "https://images-eu.ssl-images-amazon.com/images/I/71ncGHw19PL._AC_UL200_SR200,200_.jpg",
+    objectId: 3,
+  },
+  {
+    title: "Il nuovo Devoto-Oli junior. Il mio primo vocabolario di italiano.",
+    author: "Giacomo Devoto",
+    imgURL:
+      "https://images-eu.ssl-images-amazon.com/images/I/71ncGHw19PL._AC_UL200_SR200,200_.jpg",
+    objectId: 4,
+  },
+  {
+    title: "Il nuovo Devoto-Oli junior. Il mio primo vocabolario di italiano.",
+    author: "Giacomo Devoto",
+    imgURL:
+      "https://images-eu.ssl-images-amazon.com/images/I/71ncGHw19PL._AC_UL200_SR200,200_.jpg",
+    objectId: 5,
+  },
+  {
+    title: "Il nuovo Devoto-Oli junior. Il mio primo vocabolario di italiano.",
+    author: "Giacomo Devoto",
+    imgURL:
+      "https://images-eu.ssl-images-amazon.com/images/I/71ncGHw19PL._AC_UL200_SR200,200_.jpg",
+    objectId: 6,
+  },
+];
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book" key={props.keye}>
+      <img src={props.imgURL} alt="" srcset="" />
+      <h1>{props.title}</h1>
+      <h4
+        style={{
+          color: "red",
+          textDecoration: "underline",
+          padding: "10px 0",
+        }}
+      >
+        {props.author.toUpperCase()}
+      </h4>
+      <h5>{props.keye}</h5>
+    </article>
+  );
+};
+
+const output = data.map((item) => (
+  <Book
+    imgURL={item.imgURL}
+    title={item.title}
+    author={item.author}
+    keye={"item-" + item.objectId}
+  />
+));
+
+function BookList() {
+  return <section className="booklist">{output}</section>;
+}
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
